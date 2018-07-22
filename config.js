7d3ba550964d2b22ff7e413d112635954650d0be
Rodolfo <migasambrosio@gmail.com>
2018-07-22 22:57:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DISCORD_TOKEN: "NDcwMzQ2MDM0NDgxMjAxMTYy.DjX6Eg.eKfHWpl9fFrxIdYSLso7WrTRy6I",
+    DISCORD_TOKEN: "YOUR_BOT_TOKEN",
     PREFIX: "-",
-    DATABASE: "mongodb://rodolfo:migas1976@ds145981.mlab.com:45981/gintoki",
+    DATABASE: "",
 }
